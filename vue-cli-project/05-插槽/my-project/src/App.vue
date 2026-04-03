73d1ad96b62f7f846ecd2360c1c806431c3fa9ac
ChenYinging <2775452114@qq.com>
2026-04-03 16:51:29 +0800

    vue插槽代码练习

---
+++ vue-cli-project/05-插槽/my-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <MyDialog>
+      你确认要删除吗？
+    </MyDialog>
+
+    <MyDialog>
+      
+    </MyDialog>
+  </div>
+</template>
+
+<script>
+import MyDialog from './components/MyDialog.vue';
+
+export default {
+  components: {
+    MyDialog
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: rgb(161, 161, 161);
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-project/06-具名插槽/my-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <MyDialog>
+      <template  v-slot:head>
+        <div>我是大标题</div>
+      </template>
+
+     <template  #content>
+        <div>内容</div>
+     </template>
+    
+      <template  #footer>
+        <button>确认</button>
+        <button>取消</button>
+      </template>
+      </MyDialog>
+
+      <MyDialog>
+        
+      </MyDialog>
+  </div>
+</template>
+
+<script>
+import MyDialog from './components/MyDialog.vue';
+
+export default {
+  components: {
+    MyDialog
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: rgb(161, 161, 161);
+  margin-top: 60px;
+}
+</style>
